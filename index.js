baa7bb11f335f233022883f6e0256f55269c556d
sranik28~ <anikshariarrahman@gmail.com>
2023-05-02 23:52:10 +0600

    img url fiexd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const chefs = require("./database/chef.json")
+const chefs = require("./dataBase/chef.json")
 const cors = require("cors")
 
 const port = process.env.PORT || 3000
@@ -14,6 +14,13 @@ app.use(express.static(publicPath))
 app.get("/", (req, res) => {
     res.send(chefs)
 })
+
+app.get("/:id", (req, res) => {
+    const id = req.params.id 
+    const singleChef = chefs.find(chef => chef._id === id) || {}
+    res.send(singleChef)
+})
+
 app.listen(port, () => {
     console.log(`this server running at ${port}`)
 })
